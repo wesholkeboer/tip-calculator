8ab4edd5d5332eccdd95ad573dbd5561e618cd84
wes <wes.holkeboer@gmail.com>
2021-08-27 10:44:13 -0400

    built out tipper component more

--- src/components/Tipper.tsx
+++ src/components/Tipper.tsx
@@ -4,12 +4,31 @@ const Tipper = () => {
   return (
     <div className="Tipper">
       <div>
-        <label htmlFor="bill">Bill</label>
-        <input type="number" name="bill" id="bill" />
+        <div>
+          <label htmlFor="bill">Bill</label>
+          <input type="number" name="bill" id="bill" />
+        </div>
+        <div>
+          <p>Select Tip %</p>
+          <button>5%</button>
+          <button>10%</button>
+          <button>15%</button>
+          <button>25%</button>
+          <button>50%</button>
+          <input type="number" name="custom" id="custom" />
+        </div>
+        <div>
+          <label htmlFor="people">Number of People</label>
+          <input type="number" name="people" id="people" />
+        </div>
       </div>
       <div>
-        <label htmlFor="people">Number of People</label>
-        <input type="number" name="people" id="people" />
+        <div>
+          <div>
+            <h2>Tip Amount</h2>
+            <p>/ person</p>
+          </div>
+        </div>
       </div>
     </div>
   );
