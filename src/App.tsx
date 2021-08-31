1f01bfc4bb25c6821b51b3e971d4a3559f4de328
wes <wes.holkeboer@gmail.com>
2021-08-31 15:28:51 -0400

    filled out form functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
+import Tipper from "./components/Tipper";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Tipper />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Tipper.tsx
+++ src/components/Tipper.tsx
@@ -1,25 +1,48 @@
+import { useState } from "react";
 import "./Tipper.css";
 
 const Tipper = () => {
+  const [bill, setBill] = useState<number>(0);
+  const [tip, setTip] = useState<number>(0);
+  const [people, setPeople] = useState<number>(0);
+
   return (
     <div className="Tipper">
       <div>
         <div>
           <label htmlFor="bill">Bill</label>
-          <input type="number" name="bill" id="bill" />
+          <input
+            type="number"
+            name="bill"
+            id="bill"
+            value={bill}
+            onChange={(e) => setBill(parseInt(e.target.value))}
+          />
         </div>
         <div>
           <p>Select Tip %</p>
-          <button>5%</button>
-          <button>10%</button>
-          <button>15%</button>
-          <button>25%</button>
-          <button>50%</button>
-          <input type="number" name="custom" id="custom" />
+          <button onClick={() => setTip(5)}>5%</button>
+          <button onClick={() => setTip(10)}>10%</button>
+          <button onClick={() => setTip(15)}>15%</button>
+          <button onClick={() => setTip(25)}>25%</button>
+          <button onClick={() => setTip(50)}>50%</button>
+          <input
+            type="number"
+            name="custom"
+            id="custom"
+            value={tip}
+            onChange={(e) => setTip(parseInt(e.target.value))}
+          />
         </div>
         <div>
           <label htmlFor="people">Number of People</label>
-          <input type="number" name="people" id="people" />
+          <input
+            type="number"
+            name="people"
+            id="people"
+            value={people}
+            onChange={(e) => setPeople(parseInt(e.target.value))}
+          />
         </div>
       </div>
       <div>
@@ -28,6 +51,14 @@ const Tipper = () => {
             <h2>Tip Amount</h2>
             <p>/ person</p>
           </div>
+          <p>{(bill * tip) / people}</p>
+        </div>
+        <div>
+          <div>
+            <h2>Total</h2>
+            <p>/ person</p>
+          </div>
+          <p>{bill / people + (bill * tip) / people}</p>
         </div>
       </div>
     </div>
